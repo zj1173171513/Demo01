ea1eb0471359910f8215dd23417d1ba164a590e7
zj1173171513 <a1173171513a@qq.com>
2017-08-14 21:19:44 +0800

    Demo文件的创建

---
+++ app/src/main/java/com/jiyun/asus/demo01/Demo.java
@@ -0,0 +1,13 @@
+package com.jiyun.asus.demo01;
+
+import android.util.Log;
+
+/**
+ * Created by ASUS on 2017/8/14.
+ */
+
+public class Demo {
+    public void send(){
+        Log.e("TAG","day03");
+    }
+}
